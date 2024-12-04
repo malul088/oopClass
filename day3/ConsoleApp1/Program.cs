dc8ba0549ef96136127d71796c75c640d7ac2d51
Racheli <58745257+malul088@users.noreply.github.com>
2024-12-04 23:33:04 +0200

    Add files via upload

---
+++ day3/ConsoleApp1/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        public class SumCalculator
+        {
+            public int sum;
+
+            public void AddToSum(int number)
+            {
+                sum += number;
+            }
+
+        }
+        
+        public class MaxCalculator
+        {
+            public int max;
+          
+       
+            public void findMax(int number)
+            {
+                max = number>max? number:max;
+            }
+
+        }
+
+        public static void Main()
+        {
+            int[] numbers = { 1, 2, 3, 4, 5 };
+            var sumCalculator = new SumCalculator();
+            var maxCalculator = new MaxCalculator();
+
+            ArrayProcessor.ProcessArray(numbers, sumCalculator.AddToSum);
+            ArrayProcessor.ProcessArray(numbers, maxCalculator.findMax);
+
+            int totalSum = sumCalculator.sum; // Returns 15
+            Console.WriteLine($"Total sum: {totalSum}");
+
+            int max = maxCalculator.max; // Returns 5
+            Console.WriteLine($"The max: {max}");
+        }
+    }
+
+
+
+
+
+}
